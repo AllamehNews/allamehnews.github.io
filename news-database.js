6a6a3c9912fa36601fb57b3e67f186f61311bfe6
IGamesI <69199901+IGamesI@users.noreply.github.com>
2022-10-07 11:22:53 +0330

    Added data support

---
+++ news-database.js
@@ -0,0 +1,56 @@
+var news_data = {
+  "news-1": {
+    title: "شروع کار  نیوز",
+    description: "meow",
+    page: "./articles/testnews.html",
+    image: "./assets/Banners/SHORATEST.jpg",
+    date: "date",
+    isHot: false,
+    isRecommended: false,
+    isSponsored: false,
+  },
+  "news-2": {
+    title: "شروع کار علامه نیوز",
+    description:
+      "علامه نیوز فعالیت خود را به عنوان تنها خبرگذاری معتبر علامه حلی 3 شروع کرد",
+    page: "./articles/testnews.html",
+    image: "./assets/Banners/SHORATEST.jpg",
+    date: "14 مهر 1401",
+    isHot: false,
+    isRecommended: false,
+    isSponsored: false,
+  },
+  "news-3": {
+    title: "شروع کار علامه نیوز",
+    description:
+      "علامه نیوز فعالیت خود را به عنوان تنها خبرگذاری معتبر علامه حلی 3 شروع کرد",
+    page: "./articles/testnews.html",
+    image: "./assets/Banners/SHORATEST.jpg",
+    date: "14 مهر 1401",
+    isHot: false,
+    isRecommended: false,
+    isSponsored: false,
+  },
+  "news-4": {
+    title: "شروع کار علامه نیوز",
+    description:
+      "علامه نیوز فعالیت خود را به عنوان تنها خبرگذاری معتبر علامه حلی 3 شروع کرد",
+    page: "./articles/testnews.html",
+    image: "./assets/Banners/SHORATEST.jpg",
+    date: "14 مهر 1401",
+    isHot: false,
+    isRecommended: false,
+    isSponsored: false,
+  },
+  "news-5": {
+    title: "شروع کار علامه نیوز",
+    description:
+      "علامه نیوز فعالیت خود را به عنوان تنها خبرگذاری معتبر علامه حلی 3 شروع کرد",
+    page: "./articles/testnews.html",
+    image: "./assets/Banners/SHORATEST.jpg",
+    date: "14 مهر 1401",
+    isHot: false,
+    isRecommended: false,
+    isSponsored: false,
+  },
+};
